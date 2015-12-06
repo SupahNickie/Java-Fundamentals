3ffe4f6a7dc1ff15236702b6dd158379c726c9a6
SupahNickie <nicholascase@live.com>
2015-12-06 09:18:46 -0800

    Allowed quote literals in CSV handling

--- csv/CsvPractice.java
+++ csv/CsvPractice.java
@@ -25,13 +25,36 @@ public class CsvPractice {
         currentLine = scanner.nextLine();
         HashMap<String, String> currentRow = new HashMap<String, String>();
         if (currentLine != null) {
-          String[] cells = currentLine.split("\\s*,\\s*");
-          for (int i = 0; i < cells.length; i++) {
-            if ((cells[i] != null) || (cells[i].length() != 0)) {
+
+          // Grabbing index of commas only outside of quoted fields
+          boolean withinQuotes = false;
+          ArrayList<Integer> commaIndices = new ArrayList<Integer>();
+          for (int j = 0; j < currentLine.length(); j++) {
+            char c = currentLine.charAt(j);
+            if (c == ',' && withinQuotes == false) {
+              commaIndices.add(j);
+            } else if (c == '\"') {
+              withinQuotes = !withinQuotes;
+            }
+          }
+
+          // Splitting string
+          ArrayList<String> cells = new ArrayList<String>();
+          int splitIdx = 0;
+          for (int idx : commaIndices) {
+            cells.add(currentLine.substring(splitIdx, idx).replaceAll("\"", ""));
+            splitIdx = idx + 1;
+          }
+          cells.add(currentLine.substring(splitIdx, currentLine.length()).replaceAll("\"", ""));
+
+          // Pairing key with string value
+          for (int i = 0; i < cells.size(); i++) {
+            if ((cells.get(i) != null) || (cells.get(i).length() != 0)) {
               if (index == 0) {
-                headers.add(cells[i].trim());
+                headers.add(cells.get(i).trim());
               } else {
-                currentRow.put(headers.get(i), cells[i].trim());
+
+                currentRow.put(headers.get(i), cells.get(i).trim());
               }
             }
           }
