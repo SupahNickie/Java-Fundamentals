cb56ad331032a7c6518af6337e36c027d65ce4cb
SupahNickie <nicholascase@live.com>
2016-01-18 16:46:38 -0800

    Adding BFS and DFS for my integer graph data type

--- data_structures/Graph.java
+++ data_structures/Graph.java
@@ -1,4 +1,10 @@
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public abstract class Graph {
   private int numVertices;
@@ -20,6 +26,79 @@ public abstract class Graph {
   public void addEdge(int v, int w) {
     implementAddEdge(v, w);
     numEdges++;
+  }
+
+  public Map<Integer,Integer> iterativeBFS(int s, int g) {
+    Queue<Integer> queue = new LinkedList<Integer>();
+    HashSet<Integer> visited = new HashSet<Integer>();
+    Map<Integer,Integer> parents = new HashMap<Integer,Integer>();
+    int curr;
+
+    queue.add(s);
+    visited.add(s);
+    while (!queue.isEmpty()) {
+      curr = queue.remove();
+      if (curr == g) { return parents; }
+      List<Integer> neighbors = getOutNeighbors(curr);
+      for (int n : neighbors) {
+        if (visited.contains(n)) {
+          continue;
+        } else {
+          visited.add(n);
+          parents.put(n, curr);
+          queue.add(n);
+        }
+      }
+    }
+    return null;
+  }
+
+  public Map<Integer,Integer> iterativeDFS(int s, int g) {
+    Stack<Integer> stack = new Stack<Integer>();
+    HashSet<Integer> visited = new HashSet<Integer>();
+    Map<Integer,Integer> parents = new HashMap<Integer,Integer>();
+    int curr;
+
+    stack.push(s);
+    visited.add(s);
+    while (!stack.isEmpty()) {
+      curr = stack.pop();
+      if (curr == g) { return parents; }
+      List<Integer> neighbors = getOutNeighbors(curr);
+      for (int n : neighbors) {
+        if (visited.contains(n)) {
+          continue;
+        } else {
+          visited.add(n);
+          parents.put(n, curr);
+          stack.push(n);
+        }
+      }
+    }
+    return null;
+  }
+
+  public Map<Integer,Integer> recursiveDFS(int s, int g) {
+    HashSet<Integer> visited = new HashSet<Integer>();
+    Map<Integer,Integer> parents = new HashMap<Integer,Integer>();
+
+    return recursiveDFS(s, g, visited, parents);
+  }
+
+  private Map<Integer,Integer> recursiveDFS(int s, int g, HashSet<Integer> visited, Map<Integer,Integer> parents) {
+    if (s == g) {
+      return parents;
+    }
+
+    List<Integer> neighbors = getOutNeighbors(s);
+    for (int n : neighbors) {
+      if (!visited.contains(n)) {
+        visited.add(n);
+        parents.put(n, s);
+        return recursiveDFS(n, g, visited, parents);
+      }
+    }
+    return null;
   }
 
   public abstract void implementAddVertex();
