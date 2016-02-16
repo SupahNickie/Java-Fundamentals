d38c34c3dc6f32507c153ca438f7b3e340730f9c
SupahNickie <nicholascase@live.com>
2016-02-15 22:28:19 -0800

    Add Array Transpose challenge

---
+++ assorted_challenges/ArrayTranspose.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayTranspose {
+
+  public static List<ArrayList<Integer>> transpose(int[][] args) {
+    int height = args.length;
+    int width = args[0].length;
+    List<ArrayList<Integer>> container = new ArrayList<ArrayList<Integer>>();
+
+    for(int i = 0; i < width; i++) {
+      ArrayList<Integer> subContainer = new ArrayList<Integer>();
+      container.add(subContainer);
+    }
+
+    for(int i = 0; i < width; i++) {
+      List<Integer> sub = container.get(i);
+      for(int j = 0; j < height; j++) {
+        sub.add(args[j][i]);
+      }
+    }
+
+    return container;
+  }
+
+  public static void main(String[] args) {
+    int[][] things = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int[][] things2 = {{1, 2, 3}, {4, 5, 6}};
+    System.out.println(transpose(things));
+    System.out.println(transpose(things2));
+  }
+}
